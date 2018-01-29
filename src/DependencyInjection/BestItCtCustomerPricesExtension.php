23d28ee870ba21eafe0f6833b0eaf52d07299013
andre.varelmann <andre.varelmann@bestit-online.de>
2018-01-29 14:05:32 +0100

    Resolve dependencies for symfony flex

--- src/DependencyInjection/BestItCtCustomerPricesExtension.php
+++ src/DependencyInjection/BestItCtCustomerPricesExtension.php
@@ -4,8 +4,8 @@ namespace BestIt\CtCustomerPricesBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
  * Loads the config for the app bundle.
